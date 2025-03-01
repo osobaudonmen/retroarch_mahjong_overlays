e0eb2aa72ef8d211005ef2344066d67d2561fa7f
yuichietsu <osoba.udon.men@gmail.com>
2025-03-01 16:50:39 +0900

    mameのバージョンが上がった時にREADMEの更新を自動でできるようにする

--- tools/find_mah_ctrl.php
+++ tools/find_mah_ctrl.php
@@ -6,17 +6,73 @@ require_once(__DIR__ . '/Classes/MameDatFile.php');
 
 use OsobaTool\Classes\MameDatFile;
 
-$datFile = $argv[1];
+$datFile    = $argv[1];
+$readmeFile = $argv[2] ?? null;
 
 if (!file_exists($datFile)) {
     echo "$datFile not found.\n";
     exit(1);
 }
 
+$readmeGames = [];
+
+if ($readmeFile) {
+    if (!file_exists($readmeFile)) {
+        echo "$readmeFile not found.\n";
+        exit(1);
+    }
+    $inList = false;
+    $inBody = false;
+    foreach (file($readmeFile) as $line) {
+        $line = trim($line);
+        if ('<!-- mame_games -->' === $line) {
+            $inList = true;
+        } elseif ('<!-- /mame_games -->' === $line) {
+            break;
+        } elseif ($inList && str_starts_with($line, '|-')) {
+            $inBody = true;
+        } elseif ($inBody && $line) {
+            $data = explode('|', $line);
+            $name = trim($data[1] ?? '');
+            $name && ($readmeGames[$name] = $data);
+        }
+    }
+}
+
 $games = MameDatFile::readGames($datFile, ['playable', 'original', 'mahjong']);
 
-foreach ($games as $game) {
-    $name = (string)$game['name'];
-    $desc = (string)$game->description;
-    echo "$name, $desc\n";
+if ($readmeFile) {
+    $table = [
+        '<!-- mame_games -->',
+        '',
+        '|ROMファイル名|説明|オーバーレイファイル|備考|',
+        '|---|---|---|---|',
+    ];
+    foreach ($games as $game) {
+        $name = (string)$game['name'];
+        $desc = (string)$game->description;
+        if ($data = ($readmeGames[$name] ?? false)) {
+            $data[1] = $name;
+            $data[2] = $desc;
+            $table[] = implode('|', $data);
+        } else {
+            $table[] = "|$name|$desc|";
+        }
+    }
+    $table[] = '';
+    $table[] = '<!-- /mame_games -->';
+    $text = file_get_contents($readmeFile);
+    $text = preg_replace(
+        '#<!-- mame_games -->.*?<!-- /mame_games -->#s',
+        implode("\n", $table),
+        $text
+    );
+    file_put_contents($readmeFile, $text);
+    echo "updated $readmeFile.\n";
+} else {
+    foreach ($games as $game) {
+        $name = (string)$game['name'];
+        $desc = (string)$game->description;
+        echo "$name, $desc\n";
+    }
 }
